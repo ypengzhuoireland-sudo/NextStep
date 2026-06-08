1b2aa8fd896e36c9ad3592289a3d3db9ef97f8e5
ypengzhuoireland-sudo <ypengzhuo.ireland@gmail.com>
2026-06-08 21:33:49 +0800

    Remove frontend folder

--- frontend/src/api/tutor.ts
+++
@@ -1,7 +0,0 @@
-export { getClassDashboardSummary } from "@/api/dashboard";
-export { getExerciseById, getExercises } from "@/api/exercises";
-export { requestHint } from "@/api/hints";
-export { getStudentMastery } from "@/api/mastery";
-export { getNextRecommendation } from "@/api/recommendations";
-export { createSession, getCurrentPracticeSession } from "@/api/sessions";
-export { runCode, submitCode } from "@/api/submissions";
